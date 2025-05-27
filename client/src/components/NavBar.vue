f55a524f1d8b5e392093308d37752bc810d82f8c
Roma <kovachroman0704@gmail.com>
2025-05-27 08:22:51 +0300

    frontend init

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-light bg-light p-3 position-fixed top-0 w-100"
+  >
+    <div class="container-fluid">
+      <router-link class="navbar-brand fw-bolder" to="/">PhoneDoc</router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="/">Головна</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="/about"
+              >Про нас</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="/contact"
+              >Контакти</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.navbar {
+  background-color: #f8f9fa !important;
+  z-index: 1000;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="main-wrap mt-5">
+    <section
+      class="hero-section bg-dark text-white d-flex align-items-center position-relative"
+    >
+      <div class="container">
+        <div class="content row align-items-center">
+          <div class="col-md-6 text-center text-md-start">
+            <p class="text-white-50 mb-2">Welcome.Pro</p>
+            <h1 class="display-1 fw-bold mb-3">Phone Catalog Number One</h1>
+            <p class="lead mb-4 text-wrap">
+              PhoneDoc is a web application that provides a comprehensive
+              catalog of mobile phones.
+            </p>
+            <button class="btn btn-outline-light btn-lg">Get Started</button>
+          </div>
+          <div class="image-container col-md-6 position-relative">
+            <img
+              src="https://raw.githubusercontent.com/hdpngworld/HPW/main/uploads/6503838502c49-iphone%2015%20blue.png"
+              alt="Phone Catalog"
+              class="img-fluid rounded custom-img"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+
+.hero-section {
+  background: linear-gradient(135deg, #070009, #2c0227);
+  height: 95vh;
+  padding: 80px 0;
+  overflow: hidden;
+
+  .container {
+    max-width: 1500px;
+    width: 100%;
+    padding: 0 15px;
+
+    .content {
+      .text-wrap {
+        overflow-wrap: break-word;
+        max-width: 100%;
+      }
+
+      .image-container {
+        position: relative;
+        height: 100vh;
+
+        .custom-img {
+          position: absolute;
+          bottom: -50px;
+          right: 0;
+          max-width: 100%;
+          height: auto;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 40px 20px;
+    text-align: center;
+
+    .container {
+      .content {
+        .image-container {
+          height: auto;
+
+          .custom-img {
+            position: relative;
+            bottom: auto;
+            right: auto;
+            max-width: 70%;
+            margin: 20px auto 0;
+            display: block;
+          }
+        }
+
+        .display-1 {
+          font-size: 2.5rem;
+        }
+
+        .lead {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 576px) {
+    .container {
+      .content {
+        .display-1 {
+          font-size: 2rem;
+        }
+
+        .btn-lg {
+          padding: 8px 16px;
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+}
+</style>

12456f88677619474e5668fa41b026c3f587b035
Roma <kovachroman0704@gmail.com>
2025-05-27 08:22:51 +0300

    frontend init

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav-bar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>

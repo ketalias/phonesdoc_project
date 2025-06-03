1d22c6c2419e4ac9329632cec6dd1211416e379f
Roma <kovachroman0704@gmail.com>
2025-06-03 21:35:45 +0300

    style: Standardize quotes in api.js for consistency

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3000',
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:3000",
 });
 
 export default API;
